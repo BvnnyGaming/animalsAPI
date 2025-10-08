f6e747b096de1a9b14fc25b6bfa499fbf00caee0
Evan Spiering <Evanspiering@gmail.com>
2025-10-08 15:28:19 -0400

    Init

---
+++ bunnyapi/src/main/java/com/example/bunnyapi/BunnyapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bunnyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunnyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunnyapiApplication.class, args);
+	}
+
+}
---
+++ bunnyapi/src/test/java/com/example/bunnyapi/BunnyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bunnyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunnyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
